37ae97f2286317813b32f256eef4e6acef962741
jocerdas25 <georgedevcode@users.noreply.github.com>
2023-06-29 16:38:41 -0600

    First implementation of MainWindow component

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
+#https://doc.qt.io/qtforpython-6/PySide6/QtWidgets/QMainWindow.html#qmainwindow
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from QComponents.MainWindow import MainWindow
+from PySide6.QtWidgets import QApplication
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World!", alignment=QtCore.Qt.AlignCenter)
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.button.clicked.connect(self.magic)
+app = QApplication(sys.argv)
 
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+window = MainWindow()
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    widget = MyWidget()
-    widget.resize(800,800)
-    widget.show()
-    sys.exit(app.exec_())
+window.show()
+
+app.exec()
