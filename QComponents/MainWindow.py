74281f16a46e398f6767be23bbc1a79d48f9057c
jocerdas25 <georgedevcode@users.noreply.github.com>
2023-07-11 16:51:03 -0600

    new code

--- QComponents/MainWindow.py
+++ QComponents/MainWindow.py
@@ -1,7 +1,17 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import (QMainWindow, QMenu, QlineEdit)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyExplorer")
         self.setFixedSize(1024, 800)
+
+        #Menu
+        menuBar = self.menuBar()
+        fileMenu = QMenu("&File", self)
+        menuBar.addMenu(fileMenu)
+        editMenu = menuBar.addMenu("&Edit")
+        helpMenu = menuBar.addMenu("&Help") 
+
+        self.searchBar = QlineEdit()
+        
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #https://doc.qt.io/qtforpython-6/PySide6/QtWidgets/QMainWindow.html#qmainwindow
 import sys
-from QComponents.MainWindow import MainWindow
 from PySide6.QtWidgets import QApplication
+from QComponents.MainWindow import MainWindow
+
 
 app = QApplication(sys.argv)
 
