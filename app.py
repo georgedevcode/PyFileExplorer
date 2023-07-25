e3265a518abcae68d4aa1ae0c8c6609a1bc2fa35
jocerdas25 <georgedevcode@users.noreply.github.com>
2023-07-24 19:46:07 -0600

    Implementado la vista de los folders y archivos del directorio 'Home' del usuario

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 import os
 import sys
 from PyQt6 import QtCore, QtGui, QtWidgets, uic
+from PyQt6.QtGui import QFileSystemModel
+from pathlib import Path
 
 class PyFileExplorer(QtWidgets.QMainWindow):
+
+        HOME_PATH = Path.home().as_posix()
+
         def __init__(self):
             super(PyFileExplorer, self).__init__()
             self.ui_path = os.path.dirname(os.path.abspath(__file__))
             uic.load_ui.loadUi(os.path.join(self.ui_path,"PyFileExplorer.ui"), self)
+            self.FilesTreeView = self.findChild(QtWidgets.QTreeView, "FilesTreeView")
+            self.SearchButton = self.findChild(QtWidgets.QPushButton, "SearchButton")
+            self.FileViewWidget = self.findChild(QtWidgets.QListView, "FileViewWidget")
+            self.SearchFileWidget = self.findChild(QtWidgets.QLineEdit, "SearchFileWidget")
 
+            # Setting up the File View Tree
+            self.model = QFileSystemModel()
+            self.model.setRootPath(self.HOME_PATH)
+            self.FilesTreeView.setModel(self.model)
+            self.FilesTreeView.setRootIndex(self.model.index(self.HOME_PATH))
+            self.FilesTreeView.setColumnWidth(0, 250)
+            self.FilesTreeView.setAlternatingRowColors(True)
+            
 
-app = QtWidgets.QApplication(sys.argv)
-PyFileExplorer = PyFileExplorer()
-PyFileExplorer.show()
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    PyFileExplorer = PyFileExplorer()
+    PyFileExplorer.show()
+    app.exec()
