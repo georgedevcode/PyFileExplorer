35093bde7a25721275be2c9a123697fcfdcbd408
jocerdas25 <georgedevcode@users.noreply.github.com>
2023-08-04 15:44:09 -0600

    Terminando de implementar las funcionalidades basicas

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
 import sys
 from PyQt6 import QtCore, QtGui, QtWidgets, uic
-from PyQt6.QtGui import QFileSystemModel
+from PyQt6.QtCore import QUrl
+from PyQt6.QtCore import QEvent
+from PyQt6.QtGui import QFileSystemModel, QMouseEvent, QDesktopServices, QPointingDevice
 from pathlib import Path
+from PyQt6.QtWidgets import QApplication, QMainWindow, QListView, QMessageBox
+from PyQt6.QtCore import QDir, QModelIndex
+from PyQt6.QtGui import QDesktopServices
 
 class PyFileExplorer(QtWidgets.QMainWindow):
 
         HOME_PATH = Path.home().as_posix()
+        # Field to keep track on the previous index
+        PREV_INDEX_FILE_VIEW = HOME_PATH
+        
 
         def __init__(self):
             super(PyFileExplorer, self).__init__()
@@ -16,6 +24,8 @@ class PyFileExplorer(QtWidgets.QMainWindow):
             self.SearchButton = self.findChild(QtWidgets.QPushButton, "SearchButton")
             self.FileViewWidget = self.findChild(QtWidgets.QListView, "FileViewWidget")
             self.SearchFileWidget = self.findChild(QtWidgets.QLineEdit, "SearchFileWidget")
+            self.HomeButton = self.findChild(QtWidgets.QPushButton, "HomeButton")
+            self.ReturnButton = self.findChild(QtWidgets.QPushButton, "ReturnButton")
 
             # Setting up the File View Tree
             self.model = QFileSystemModel()
@@ -24,7 +34,35 @@ class PyFileExplorer(QtWidgets.QMainWindow):
             self.FilesTreeView.setRootIndex(self.model.index(self.HOME_PATH))
             self.FilesTreeView.setColumnWidth(0, 250)
             self.FilesTreeView.setAlternatingRowColors(True)
-            
+            self.FilesTreeView.doubleClicked.connect(self.MouseDoubleClickOpenFileFolders)
+
+            # Setting up the File View tree
+            self.FileViewWidget.setModel(self.model)
+            self.FileViewWidget.setRootIndex(self.model.index(self.HOME_PATH))
+            self.FileViewWidget.setViewMode(self.FileViewWidget.ViewMode.IconMode)
+            self.FileViewWidget.setResizeMode(self.FileViewWidget.ResizeMode.Adjust)
+            self.FileViewWidget.setMovement(self.FileViewWidget.Movement.Free)
+
+            #slots
+            self.HomeButton.clicked.connect(self.HomeButtonClickEvent)
+            self.FileViewWidget.doubleClicked.connect(self.MouseDoubleClickOpenFileFolders)
+        
+        def MouseDoubleClickOpenFileFolders(self, index):
+                file_info = self.model.fileInfo(index)
+                if file_info.isDir():
+                    # If it's a folder, open it
+                    folder_path = file_info.absoluteFilePath()
+                    self.model.setRootPath(folder_path)
+                    self.FileViewWidget.setRootIndex(self.model.index(folder_path))
+                else:
+                    # If it's a file, open it with the default application
+                    file_path = file_info.filePath()
+                    QDesktopServices.openUrl(QUrl.fromLocalFile(file_path))
+        
+        def HomeButtonClickEvent(self):
+             self.model.setRootPath(self.HOME_PATH)
+             self.FileViewWidget.setRootIndex(self.model.index(self.HOME_PATH))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
