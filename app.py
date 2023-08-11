b7a789ffa7cb9d779d235b11d74e967fd08b6df9
FOX-151711 <fofo15-93@hotmail.com>
2023-08-11 12:28:41 -0600

    Rodolfo Arroyo Cordero

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import sys
 from PyQt6 import QtCore, QtGui, QtWidgets, uic
 from PyQt6.QtCore import QUrl
 from PyQt6.QtCore import QEvent
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFileSystemModel, QMouseEvent, QDesktopServices, QPointingDevice
 from pathlib import Path
-from PyQt6.QtWidgets import QApplication, QMainWindow, QListView, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QListView, QMessageBox, QMenu, QInputDialog
 from PyQt6.QtCore import QDir, QModelIndex
 from PyQt6.QtGui import QDesktopServices
 
@@ -27,7 +28,7 @@ class PyFileExplorer(QtWidgets.QMainWindow):
             self.HomeButton = self.findChild(QtWidgets.QPushButton, "HomeButton")
             self.ReturnButton = self.findChild(QtWidgets.QPushButton, "ReturnButton")
             self.SearchButton.clicked.connect(self.SearchButtonClickEvent)
-
+                        
             # Setting up the File View Tree
             self.model = QFileSystemModel()
             self.model.setRootPath(self.HOME_PATH)
@@ -47,6 +48,91 @@ class PyFileExplorer(QtWidgets.QMainWindow):
             #slots
             self.HomeButton.clicked.connect(self.HomeButtonClickEvent)
             self.FileViewWidget.doubleClicked.connect(self.MouseDoubleClickOpenFileFolders)
+
+#Rodolfo
+            # Crear el menú
+            self.menu = QMenu(self)
+
+            # Agregar acciones al menú
+            self.action1 = self.menu.addAction("Crear Archivo")
+            self.action2 = self.menu.addAction("Modificar Archivo")
+            self.action3 = self.menu.addAction("Eliminar Archivo")
+
+            # Conectar las acciones a sus respectivas funciones
+            self.action1.triggered.connect(self.create_new_file)
+            self.action2.triggered.connect(self.modify_existing_file)
+            self.action3.triggered.connect(self.delete_existing_file)
+            #fin
+            
+#Rodolfo
+        def mousePressEvent(self, event):
+            if event.button() == Qt.RightButton:
+                # Mostrar el menú en la posición del cursor
+                self.menu.exec_(event.globalPos())
+
+        def on_action1_triggered(self):
+            self.create_new_file()
+
+        def on_action2_triggered(self):
+            self.modify_file()
+            
+        def on_action3_triggered(self):
+            print("Acción 3")   
+            
+        def create_file(self, filename):
+            current_path = self.model.rootPath()
+            file_path = os.path.join(current_path, filename)
+    
+            if not os.path.exists(file_path):
+                with open(file_path, 'w') as new_file:
+                    pass  
+            
+                self.model.refresh(self.model.index(current_path))
+        
+        def create_new_file(self):
+            filename, ok = QInputDialog.getText(self, 'Crear Archivo', 'Ingrese el nombre del archivo:')
+            if ok and filename:
+                self.create_file(filename)
+        
+        def file_exists(self, filename):
+            current_path = self.model.rootPath()
+            file_path = os.path.join(current_path, filename)
+            return os.path.exists(file_path)
+
+        def modify_file(self, filename):
+            if not self.file_exists(filename):
+                QMessageBox.warning(self, 'Archivo no encontrado', f'El archivo "{filename}" no existe en este directorio.')
+            return
+    
+            current_path = self.model.rootPath()
+            file_path = os.path.join(current_path, filename)
+
+            self.model.refresh(self.model.index(current_path))
+
+        def modify_existing_file(self):
+            filename, ok = QInputDialog.getText(self, 'Modificar Archivo', 'Ingrese el nombre del archivo a modificar:')
+            if ok and filename:
+                self.modify_file(filename)
+
+        def delete_file(self, filename):
+            current_path = self.model.rootPath()
+            file_path = os.path.join(current_path, filename)
+    
+            if os.path.exists(file_path):
+                try:
+                    os.remove(file_path)
+                    # Refresca la vista para reflejar los cambios
+                    self.model.refresh(self.model.index(current_path))
+                except OSError as e:
+                    QMessageBox.critical(self, 'Error al Eliminar', f"No se pudo eliminar el archivo '{filename}': {e}")
+            else:
+                    QMessageBox.warning(self, 'Archivo no Encontrado', f"El archivo '{filename}' no existe.")
+
+        def delete_existing_file(self):
+            filename, ok = QInputDialog.getText(self, 'Eliminar Archivo', 'Ingrese el nombre del archivo a eliminar:')
+            if ok and filename:
+                self.delete_file(filename)
+#fin
 
         def MouseDoubleClickOpenFileFolders(self, index):
                 file_info = self.model.fileInfo(index)
